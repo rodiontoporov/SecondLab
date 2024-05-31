e5a1935d0bca1234bd073686928968bda83ae81f
rodiontoporov <rodiontoporov@icloud.com>
2024-05-31 21:26:38 +0300

    started working

---
+++ SecondLab/BinarySearchTree.cpp
@@ -0,0 +1,8 @@
+//
+//  BinarySearchTree.cpp
+//  SecondLab
+//
+//  Created by Guest User on 31.05.2024.
+//
+
+#include "BinarySearchTree.hpp"
---
+++ SecondLab/BinarySearchTree.hpp
@@ -0,0 +1,13 @@
+//
+//  BinarySearchTree.hpp
+//  SecondLab
+//
+//  Created by Guest User on 31.05.2024.
+//
+
+#ifndef BinarySearchTree_hpp
+#define BinarySearchTree_hpp
+
+#include <stdio.h>
+
+#endif /* BinarySearchTree_hpp */
