6f9d4bceea9db1e37d9f3b436d170d8f854d34f8
rodiontoporov <rodiontoporov@icloud.com>
2024-06-02 19:50:07 +0300

    dvasdvdsvgereg

--- SecondLab/BinarySearchTree.hpp
+++ SecondLab/BinarySearchTree.hpp
@@ -1,38 +1,7 @@
-#include <iostream>
-
-namespace my {
+#include "BinarySearchTreeNode.h"
 struct PreOrderTag {};
 struct InOrderTag {};
 struct PostOrderTag {};
-template<
-    typename Key,
-    typename Alloc = std::allocator<Key>>
-class BST_Node {
-public:
-    using key_type = Key;
-    using value_type = Key;
-    using allocator_type = Alloc;
-    
-private:
-    using container_node_type = BST_Node<Key, Alloc>;
-    using ator_traits = std::allocator_traits<allocator_type>;
-    
-    typename ator_traits::template rebind_traits<container_node_type>::pointer ptr_;
-    std::optional<allocator_type> alloc_;
-public:
-    // constructors, copy and assignment
-    constexpr BST_Node() noexcept : ptr_(), alloc_() {}
-    BST_Node(BST_Node&&) noexcept;
-    BST_Node& operator=(BST_Node&&);
-    
-    // destructor
-    ~BST_Node();
-    
-    // observers
-    value_type& value() const;
-    
-    allocator_type get_allocator() const;
-};
 template<
     typename Key,
     typename Alloc = std::allocator<Key>,
@@ -46,5 +15,29 @@ class BST {
     using key_compare = Compare; // CopyConstructible
     using value_compare = Compare;
     using node_type = BST_Node<Key, Alloc>; // ? // Public nested types are the same
+    
+    BST(key_compare c) : c_(c) {}
+    BST() = default;
+    BST(auto i, auto j, key_compare c) : c_(c) 
+    { 
+        for (; i != j; ++i) { } /* do sth */
+    }
+    BST(auto i, auto j)
+    {
+        for (; i != j; ++i) { } /* do sth */
+    }
+    BST(std::initializer_list<value_type> il) 
+    {
+        BST(il.begin(), il.end());
+    }
+    BST(std::initializer_list<value_type> il, key_compare c)
+    {
+        BST(il.begin(), il.end(), c);
+    }
+    
+    
+private:
+    Compare c_;
+    
 };
-}; // end of namespace my
+
